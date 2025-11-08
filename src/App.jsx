c87a85990325a0c4892434f51c7e3c38cfd8056e
Yash Srivastava <ysyashmks@gmail.com>
2025-11-08 14:49:03 +0530

    page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./Components/MainPage.jsx";
+import ScrollToTop from "./Components/UI/ScrollToTop.jsx";
+import AboutSection from "./Components/AboutSection.jsx";
+import "./index.css";
+import Mentor from "./Components/Mentor.jsx";
+import Auth from "./Components/Auth.jsx";
+import ProtectedRoute from "./Components/ProtectedRoute.jsx";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="relative w-full h-full min-h-screen overflow-x-hidden bg-white">
+      {/* Squares always in the background */}
+      
+
+      <Router>
+           <ScrollToTop />
+        <Routes>
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/" element={<MainPage />} />
+            <Route
+      path="/interview"
+      element={
+        <ProtectedRoute>
+          <Mentor />
+        </ProtectedRoute>
+      }
+    />
+          {/* Add other routes as needed */}
+        </Routes>
+      </Router>
+      {/* Footer or additional sections can be added here */}
+    </div>
+  );
 }
 
-export default App
+export default App;
